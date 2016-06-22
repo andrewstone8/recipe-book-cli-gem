d315f058601a525a392923e01f064599512cfedd
andrewstone8 <andrewstone8@gmail.com>
2016-06-22 20:15:47 +0900

    added call, welcome, and list_cuisines methods

--- lib/recipe_book/cli.rb
+++ lib/recipe_book/cli.rb
@@ -4,10 +4,25 @@ class RecipeBook::CLI
 
 
   def call
-    puts "hello world"
+    welcome
+    list_cuisines
   end
 
+  def welcome
+    puts "---------- Welcome to Andrew's Recipe Book ----------"
+    puts "             (courtesy of blueapron.com)"
+    puts ''
+  end
 
+  def list_cuisines
+    puts "What type of food are you in the mood for?"
+    puts "1. Greek"
+    puts "2. Italian"
+    puts "3. Korean"
+    puts "4. Mexican"
+    puts "5. Southern"
+    puts ""
+  end
 
 
 
